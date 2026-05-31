7ca03ea33bff15109685182228c399067a40c155
jgabrielss <jgkwy07@gmail.com>
2026-05-31 14:44:19 -0300

    1.0 stable

--- viteconfig.ts
+++
@@ -1,9 +0,0 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-  export default defineConfig({
-    plugins: [
-      vinext(),
-      cloudflare({
-        viteEnvironment: { name: "rsc", childEnvironments: ["ssr"] },
-      }),
-    ],
-  });
